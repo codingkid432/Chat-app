abc02f8551b752ad8ab8d6b0800c26392974e44e
codingkid432 <75382130+codingkid432@users.noreply.github.com>
2021-04-14 19:57:08 -0500

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,1 +1,10 @@
-js
+var firebaseConfig = {
+    apiKey: "AIzaSyAip6A8lK7Unzna-BV1DVkeAbcQ2Lsk4rc",
+    authDomain: "chat-app-c2e6b.firebaseapp.com",
+    projectId: "chat-app-c2e6b",
+    storageBucket: "chat-app-c2e6b.appspot.com",
+    messagingSenderId: "213894158443",
+    appId: "1:213894158443:web:be0b476fdd84726392f92e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function addUser(){
-    user_name=document.getElementById("user_name").value;
-    localStorage.setItem("User_name",user_name);
-    window.location="kwitter.js";
+    user_name=document.getElementById("username").value;
+    localStorage.setItem("Username",user_name);
+    window.location="kwitter.html";
 }
